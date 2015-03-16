a0243e474f04d6c18e8f30b54b2163d924b6b5be
Ming Yeung <ming.yeung@npnf.com>
2015-03-16 16:00:31 -0700

    support all the http method during callback

--- src/main/java/com/nearform/quartz/JobData.java
+++ src/main/java/com/nearform/quartz/JobData.java
@@ -4,18 +4,21 @@ public class JobData {
 
   private String url;
   private String name;
+  private String method;
   private String group;
   private String payload;
   private long timestamp;
 
   public String getUrl() { return url; }
   public String getName() { return name; }
+  public String getMethod() { return method; }
   public String getGroup() { return group; }
   public String getPayload() { return payload; }
   public long getTimestamp() { return timestamp; }
 
   public void setUrl(String s) { url = s; }
   public void setName(String s) { name = s; }
+  public void setMethod(String s) { method = s; }
   public void setGroup(String s) { group = s; }
   public void setPayload(String s) { payload = s; }
   public void setTimestamp(long i) { timestamp = i; }
